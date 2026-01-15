8606bb617d699375647117a421095296b2ce9f39
Saba126 <vsaba126.dev@gmail.com>
2026-01-15 06:33:44 +0100

    First get

--- 01_PRIMERAS_RUTAS/index.js
+++ 01_PRIMERAS_RUTAS/index.js
@@ -1,4 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.get("/saludo",(req,res) =>{
+  res.status(200).send("Hola me llamo saba y quiero ser programador")
+})
+
 app.listen(3000, () => console.log("proyecto arrancado por el puert 3000"))
